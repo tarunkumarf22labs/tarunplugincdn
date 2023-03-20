26ef0cb4d5ad3093689a55fc49824d69a17459f1
tarunkumarf22labs <119585731+tarunkumarf22labs@users.noreply.github.com>
2023-03-20 15:59:44 +0530

    changes in ocenus production

--- src/main.tsx
+++ src/main.tsx
@@ -11,5 +11,5 @@ define(
 );
 
 let el = document.createElement('f22-plugin')
-el.setAttribute('dataUrl', "https://f22videoplugin.s3.ap-northeast-1.amazonaws.com/lapp/lapp.json")
+el.setAttribute('dataUrl', "https://f22videoplugin.s3.ap-northeast-1.amazonaws.com/Oceanus/ocenus.json")
 document.body.append(el)
