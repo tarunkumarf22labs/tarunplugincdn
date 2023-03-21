f11f5ab906c74def8bdc9e59699a7e3fccede235
tarunkumarf22labs <119585731+tarunkumarf22labs@users.noreply.github.com>
2023-03-21 10:52:14 +0530

    last changes

--- src/main.tsx
+++ src/main.tsx
@@ -11,5 +11,5 @@ define(
 );
 
 let el = document.createElement('f22-plugin')
-el.setAttribute('dataUrl', "https://f22videoplugin.s3.ap-northeast-1.amazonaws.com/lapp/lapp.json")
+el.setAttribute('dataUrl', "https://f22videoplugin.s3.ap-northeast-1.amazonaws.com/Oceanus/ocenus1.json")
 document.body.append(el)
