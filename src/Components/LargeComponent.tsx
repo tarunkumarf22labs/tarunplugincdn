62b8bce412b3d27b71775b05e44e39f7e9b298d1
tarunkumarf22labs <119585731+tarunkumarf22labs@users.noreply.github.com>
2023-03-21 13:28:15 +0530

    handle errors

--- src/Components/LargeComponent.tsx
+++ src/Components/LargeComponent.tsx
@@ -45,15 +45,15 @@ function LargeComponent({
 
 
 
-  useEffect(() => {
-    setInterval(() => {
-      let vals =
-        (videoEl.current!?.currentTime / videoEl.current!?.duration) * 96;
-         console.log(vals);
+  // useEffect(() => {
+  //   setInterval(() => {
+  //     let vals =
+  //       (videoEl.current!?.currentTime / videoEl.current!?.duration) * 96;
+  //        console.log(vals);
          
-      setCurrentTime( () => vals);
-    }, 100);
-  }, []);
+  //     setCurrentTime( () => vals);
+  //   }, 100);
+  // }, []);
 
   // const handleProgress = (e: any) => {
   //   if (isNaN(e.target.duration))
@@ -213,7 +213,7 @@ function LargeComponent({
           className="lg-video-for-full"
           playsInline
           start={0}
-          
+
         />
         {true ? (
           <div className="pausestyles" onClick={() => handlepause()}>
