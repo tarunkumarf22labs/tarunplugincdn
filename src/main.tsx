0fc46a50435d1644c276022979b6cc3b00bbe336
tarunkumarf22labs <119585731+tarunkumarf22labs@users.noreply.github.com>
2023-03-22 10:45:09 +0530

    changes in the pause

--- src/main.tsx
+++ src/main.tsx
@@ -11,5 +11,5 @@ define(
 );
 
 let el = document.createElement('f22-plugin')
-el.setAttribute('dataUrl', "https://f22videoplugin.s3.ap-northeast-1.amazonaws.com/paperlondon/paperlondon.json")
+el.setAttribute('dataUrl', "https://f22videoplugin.s3.ap-northeast-1.amazonaws.com/Oceanus/ocenus2.json")
 document.body.append(el)
